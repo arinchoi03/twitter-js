103a772b6c812c49c7b1e049604e384bfac0d7b6
Yejee Arin Choi <yej.arin.choi@gmail.com>
2017-03-06 18:31:01 -0500

    twitter-js in progress

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require( 'express' );
+const chalk = require('chalk');
 const app = express(); // creates an instance of an express application
+const volleyball = require('volleyball'); //does volleyball need express to run before?
+
+app.use(function(req,res,next) {
+  console.log(chalk.red(req.method + req.path + res.statusCode))
+  next();
+})
+
+app.use('/special/', function(req, res, next) {
+  console.log("you reached the special area")
+  next();
+})
 
 app.get('/', function(req, res) {
-  res.send("Welcome!")
+  res.send("Welcome home!")
 })
+
+app.get('/news', function(req, res) {
+  res.send("Welcome to NEWS!")
+})
+
+
 
 var server = app.listen(3000, () => {console.log("Server listening")})
